910c9688eafc3a3cf2fb2a01a99a278e48ae1977
carbolymer <carbolymer@dropacid.pl>
2012-07-17 11:30:44 +0200

    ...

--- run.sh
+++ run.sh
@@ -7,39 +7,39 @@ IFS="
 "
 # Kaons
 find $1 -name "outfilekkcf*" -type f | sort | ./merger 1> filelist.kk.in
-FILES=`cat filelist.kk.in`
-for parameter in $FILES
-do
-	IFS=$IFS_BAK
-	./fitsh $parameter kk &
-	./fit1d $parameter kk &
-	IFS="
-"
-done
+# FILES=`cat filelist.kk.in`
+# for parameter in $FILES
+# do
+# 	IFS=$IFS_BAK
+# 	./fitsh $parameter kk &
+# 	./fit1d $parameter kk &
+# 	IFS="
+# "
+# done
 
 # Pions
 find $1 -name "outfilecf*" -type f | sort | ./merger 1> filelist.pipi.in
-FILES=`cat filelist.pipi.in`
-for parameter in $FILES
-do
-	IFS=$IFS_BAK
-	./fitsh $parameter pipi &
-	./fit1d $parameter pipi &
-	IFS="
-"
-done
+# FILES=`cat filelist.pipi.in`
+# for parameter in $FILES
+# do
+# 	IFS=$IFS_BAK
+# 	./fitsh $parameter pipi &
+# 	./fit1d $parameter pipi &
+# 	IFS="
+# "
+# done
 
 # Protons
 find $1 -name "outfileppcf*" -type f | sort | ./merger 1> filelist.pp.in
-FILES=`cat filelist.pp.in`
-for parameter in $FILES
-do
-	IFS=$IFS_BAK
-	./fitsh $parameter pp &
-	./fit1d $parameter pp &
-	IFS="
-"
-done
+# FILES=`cat filelist.pp.in`
+# for parameter in $FILES
+# do
+# 	IFS=$IFS_BAK
+# 	./fitsh $parameter pp &
+# 	./fit1d $parameter pp &
+# 	IFS="
+# "
+# done
 
-IFS=$IFS_BAK
-IFS_BAK=
+# IFS=$IFS_BAK
+# IFS_BAK=
